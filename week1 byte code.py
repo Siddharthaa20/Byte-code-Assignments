37a92913ca4c60357b057e2bb2f181b22ec1f5c2
Siddhartha <arepallisiddharthasiddhu@gmail.com>
2025-09-01 17:59:23 +0530

    Task 1

---
+++ week1 byte code.py
@@ -0,0 +1,13 @@
+
+principal = 5000
+rate_of_interest = 8.5 / 12   
+time_in_months = 18
+
+interest_calculated = (principal * rate_of_interest * time_in_months) / 100
+total_amount = principal + interest_calculated
+
+print("Principal Amount:$", principal)
+print("Rate of interest (per month):", rate_of_interest, "%")
+print("Time (in months):", time_in_months)
+print("Simple Interest Earned:$", interest_calculated)
+print("Total Amount in 18 months:$", total_amount)
