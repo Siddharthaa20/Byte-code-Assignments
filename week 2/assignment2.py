d6dac0a2dc3fe463693f4b75b5cb7feadcb092d9
Siddhartha <arepallisiddharthasiddhu@gmail.com>
2025-09-09 23:44:41 +0530

    week 2

---
+++ week 2/assignment2.py
@@ -0,0 +1,13 @@
+
+expenses = [1200, 1500, 800, 2500, 500, 700, 1100]
+total_expense = sum(expenses)
+highest_expense = max(expenses)
+lowest_expense = min(expenses)
+expenses.append(950)
+
+print("Total Expense: ₹",{total_expense})
+print("Highest Expense: ₹",{highest_expense})
+print("Lowest Expense: ₹",{lowest_expense})
+print("Updated Expenses List:", expenses)
+
+
