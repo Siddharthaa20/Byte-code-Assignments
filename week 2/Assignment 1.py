2d8471d064a1fc8fda6388c5da7554303f30bc4c
Siddhartha <arepallisiddharthasiddhu@gmail.com>
2025-09-11 17:26:09 +0530

    week 2

--- week 2/Assignment 1.py
+++ week 2/Assignment 1.py
@@ -2,4 +2,4 @@ product_name = "Laptop"
 quantity_sold = 12
 price_per_unit = 45000.50
 total_revenue = quantity_sold * price_per_unit
-print("Sold 12 units of Laptop for a total revenue of ₹",total_revenue)
+print("Sold", quantity_sold,"units of Laptop for a total revenue of ₹",total_revenue)
